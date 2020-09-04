8d5cdd2bd9043f39937fde49537681cd58b602e2
nishitsb <nishitsbansal1997@gmail.com>
2020-09-04 23:26:31 +0530

    Added logic for anagrams

--- anagram.cpp
+++ anagram.cpp
@@ -1,21 +1,30 @@
 #include "anagram.h"
 
-bool Anagram::WordPairIsAnagram(const std::string &word1, const std::string &word2)
+void incrementCharCount(const std::string &word1, int *charCount)
 {
-
-    int charCount[26] = {0};
-
     for (int i = 0; i < word1.length(); i++)
     {
         if (word1[i] != ' ')
             charCount[tolower(word1[i]) - 97]++;
     }
+}
 
+void decrementCharCount(const std::string &word2, int *charCount)
+{
     for (int i = 0; i < word2.length(); i++)
     {
         if (word2[i] != ' ')
             charCount[tolower(word2[i]) - 97]--;
     }
+}
+
+bool Anagram::WordPairIsAnagram(const std::string &word1, const std::string &word2)
+{
+    int charCount[26] = {0};
+
+    incrementCharCount(word1, charCount);
+
+    decrementCharCount(word2, charCount);
 
     for (int i = 0; i < 26; i++)
     {
