24a6585ec3d816402d9579f167c40284bf8a5197
nishitsb <nishitsbansal1997@gmail.com>
2020-09-04 22:09:26 +0530

    Added logic for anagrams

--- anagram.cpp
+++ anagram.cpp
@@ -1,13 +1,43 @@
 #include "anagram.h"
 
-bool Anagram::WordPairIsAnagram(const std::string& word1, const std::string& word2) {
-    //Fill the correct implementation here
-    return false;
+bool Anagram::WordPairIsAnagram(const std::string &word1, const std::string &word2)
+{
+
+    int charCount[26] = {0};
+
+    for (int i = 0; i < word1.length(); i++)
+    {
+        if (word1[i] != ' ')
+            charCount[tolower(word1[i]) - 97]++;
+    }
+
+    for (int i = 0; i < word2.length(); i++)
+    {
+        if (word2[i] != ' ')
+            charCount[tolower(word2[i]) - 97]--;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (charCount[i])
+            return false;
+    }
+
+    return true;
 }
 
 std::vector<std::string> Anagram::SelectAnagrams(
-        const std::string& word,
-        const std::vector<std::string>& candidates) {
-    //Fill the correct implementation here
-    return candidates;
+    const std::string &word,
+    const std::vector<std::string> &candidates)
+{
+    std::vector<std::string> filteredCandidates;
+
+    for (std::string s : candidates)
+    {
+        if (WordPairIsAnagram(word, s))
+        {
+            filteredCandidates.push_back(s);
+        }
+    }
+    return filteredCandidates;
 }
